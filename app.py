2e32e1191e9ac973510d3c2ce1f4357736683b8e
aleksandrakudaeva <alexandrakudaeva@gmail.com>
2022-10-19 00:42:54 +0200

    bug fix

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ gc = gs.service_account_from_dict(credentials)
 sh = gc.open_by_url(gs_url)
 worksheet = sh.get_worksheet(0)
 data = worksheet.get_all_records()
-labeled_items = data[0].unique()
+labeled_items = data['Filename'].unique()
 
 final_list = list(set(all_items) - set(labeled_items))
 
